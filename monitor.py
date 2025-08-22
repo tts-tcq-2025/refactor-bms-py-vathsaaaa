5366ccf5eb2bbd6091e41d7480fb409349342e22
vathsaaaa <m.srivathsa@in.bosch.com>
2025-08-22 10:42:12 +0530

    Update monitor.py reduce time complexity

--- monitor.py
+++ monitor.py
@@ -9,11 +9,11 @@ PULSE_RATE_HIGH_THRESHOLD = 100
 OXY_SAT_LOW_THRESHOLD = 90
 
 checks = [
-  (temperature < TEMPERATURE_LOW_THRESHOLD, "Temperature critical!"),
-  (temperature > TEMPERATURE_HIGH_THRESHOLD, "Temperature critical!"),
-  (pulseRate < PULSE_RATE_LOW_THRESHOLD, "Pulse Rate is out of range!"),
-  (pulseRate > PULSE_RATE_HIGH_THRESHOLD, "Pulse Rate is out of range!"),
-  (spo2 < OXY_SAT_LOW_THRESHOLD, "Oxygen Saturation out of range!"),
+  (lambda t, p, s: t < TEMPERATURE_LOW_THRESHOLD, "Temperature critical!"),
+  (lambda t, p, s: t > TEMPERATURE_HIGH_THRESHOLD, "Temperature critical!"),
+  (lambda t, p, s: p < PULSE_RATE_LOW_THRESHOLD, "Pulse Rate is out of range!"),
+  (lambda t, p, s: p > PULSE_RATE_HIGH_THRESHOLD, "Pulse Rate is out of range!"),
+  (lambda t, p, s: s < OXY_SAT_LOW_THRESHOLD, "Oxygen Saturation out of range!"),
 ]
 
 def vitalsOutofRange(error_messages):
@@ -28,7 +28,7 @@ def vitalsOutofRange(error_messages):
       sleep(1)
 
 def check_vitals(temperature, pulseRate, spo2):
-  return [msg for cond, msg in checks if cond]
+  return [msg for cond, msg in checks if cond(temperature, pulseRate, spo2)]
 
 def vitals_ok(temperature, pulseRate, spo2):
   error_messages = check_vitals(temperature, pulseRate, spo2)
