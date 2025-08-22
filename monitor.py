fd9e698bdf7d19f9922244653614de327d3f0efe
vathsaaaa <m.srivathsa@in.bosch.com>
2025-08-22 07:51:26 +0530

    Update monitor.py
    Reduce Cyclomatic complexity

--- monitor.py
+++ monitor.py
@@ -2,36 +2,25 @@
 from time import sleep
 import sys
 
+def vitalsOutofRange():
+  for i in range(6):
+    print('\r* ', end='')
+    sys.stdout.flush()
+    sleep(1)
+    print('\r *', end='')
+    sys.stdout.flush()
+    sleep(1)
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
+  checks = [
+      (temperature > 102 or temperature < 95, "Temperature critical!"),
+      (pulseRate < 60 or pulseRate > 100, "Pulse Rate is out of range!"),
+      (spo2 < 90, "Oxygen Saturation out of range!"),
+  ]
+  
+  for condition, message in checks:
+      if condition:
+          print(message)
+          vitalsOutofRange()
+          return False
   return True
