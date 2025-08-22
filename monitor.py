062f5da29910b8cbd53a1a8d0e900f9c2d78bc98
vathsaaaa <m.srivathsa@in.bosch.com>
2025-08-22 10:14:35 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,34 +2,38 @@
 from time import sleep
 import sys
 
-def vitalsOutofRange():
-  for i in range(6):
-    print('\r* ', end='')
-    sys.stdout.flush()
-    sleep(1)
-    print('\r *', end='')
-    sys.stdout.flush()
-    sleep(1)
+TEMPERATURE_LOW_THRESHOLD = 95
+TEMPERATURE_HIGH_THRESHOLD = 102
+PULSE_RATE_LOW_THRESHOLD = 60
+PULSE_RATE_HIGH_THRESHOLD = 100
+OXY_SAT_LOW_THRESHOLD = 90
+
+checks = [
+  (temperature < TEMPERATURE_LOW_THRESHOLD, "Temperature critical!"),
+  (temperature > TEMPERATURE_HIGH_THRESHOLD, "Temperature critical!"),
+  (pulseRate < PULSE_RATE_LOW_THRESHOLD, "Pulse Rate is out of range!"),
+  (pulseRate > PULSE_RATE_HIGH_THRESHOLD, "Pulse Rate is out of range!"),
+  (spo2 < OXY_SAT_LOW_THRESHOLD, "Oxygen Saturation out of range!"),
+]
+
+def vitalsOutofRange(error_messages):
+  for message in error_messages:
+    output(message)
+    for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
 
 def check_vitals(temperature, pulseRate, spo2):
-  checks = [
-      (temperature < 95, "Temperature critical!"),
-      (temperature > 102, "Temperature critical!"),
-      (pulseRate < 60, "Pulse Rate is out of range!"),
-      (pulseRate > 100, "Pulse Rate is out of range!"),
-      (spo2 < 90, "Oxygen Saturation out of range!"),
-  ]
 
-  # Return first failing message, or None
-  for cond, msg in checks:
-      if cond:
-          return msg
-  return None
+return [msg for cond, msg in checks if cond]
 
 def vitals_ok(temperature, pulseRate, spo2):
-  error_message = check_vitals(temperature, pulseRate, spo2)
-  if error_message:
-      print(error_message)
-      vitalsOutofRange()
-      return False
+  error_messages = check_vitals(temperature, pulseRate, spo2)
+  if error_messages:
+    vitalsOutofRange(error_message)
+    return False
   return True
