8d7ca12e3d61a45d235c1d5b0fe865346e8b6c56
vathsaaaa <m.srivathsa@in.bosch.com>
2025-08-22 09:18:02 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -4,9 +4,14 @@ from monitor import vitals_ok
 
 class MonitorTest(unittest.TestCase):
     def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
         self.assertTrue(vitals_ok(98.1, 70, 98))
+        self.assertFalse(vitals_ok(102.1, 70, 98))
+        self.assertTrue(vitals_ok(94.9, 70, 98))
+        self.assertTrue(vitals_ok(98.1, 59, 98))
+        self.assertTrue(vitals_ok(98.1, 101, 98))
+        self.assertTrue(vitals_ok(98.1, 70, 89))
 
 
 if __name__ == '__main__':
   unittest.main()
+
